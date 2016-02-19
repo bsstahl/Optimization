f09dfb9596d961e4caf759737d4d5648922f28b3
bsstahl <barry@bsstahl.com>
2016-02-19 14:30:05 -0700

    Add the XKCD Appetizers demo

---
+++ Bss.Optimization.Appetizers/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bss.Optimization.Appetizers
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+        public double Cost { get; set; }
+    }
+}
---
+++ Bss.Optimization.Appetizers/Program.cs
@@ -0,0 +1,37 @@
+﻿using Gurobi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bss.Optimization.Appetizers
+{
+    class Program
+    {
+        // Based on XKCD #287 at http://xkcd.com/287/
+
+        static void Main(string[] args)
+        {
+            var env = new GRBEnv("Menu.log");
+            var items = new MenuItemCollection();
+            var c = new Model(env, items, 15.05);
+            var results = c.Optimize();
+            DisplayResults(results, items);
+        }
+
+        private static void DisplayResults(OptimizationResults results, IEnumerable<MenuItem> items)
+        {
+            Console.WriteLine($"Objective Value: {results.ObjectiveValue}");
+            var n = results.Items.GetLength(0);
+            for (int j = 0; j < n; j++)
+            {
+                var item = items.Single(i => i.Id == j);
+                Console.WriteLine($"{item.Name}: {results.Items[j]}");
+            }
+
+            Console.WriteLine(string.Empty);
+        }
+
+    }
+}
