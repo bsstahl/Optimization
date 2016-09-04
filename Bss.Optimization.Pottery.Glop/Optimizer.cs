eaca00f0e1a5519dc9e48dc05da5c6bb179df110
bsstahl <barry@bsstahl.com>
2016-09-04 13:48:07 -0700

    Implement GetFeasibleTargets using Google solver

--- Bss.Optimization.Pottery.Glop/Optimizer.cs
+++ Bss.Optimization.Pottery.Glop/Optimizer.cs
@@ -16,55 +16,55 @@ namespace Bss.Optimization.Pottery.Glop
         // any production optimization.
         public IEnumerable<ProductionTarget> GetFeasibleTargets(double claySupply, double glazeSupply)
         {
-            //List<ProductionTarget> targets = new List<ProductionTarget>();
+            List<ProductionTarget> targets = new List<ProductionTarget>();
 
-            //// Setup the Model
-            //var solver = CreateLinearProgrammingSolver();
+            // Setup the Model
+            var solver = CreateLinearProgrammingSolver();
 
-            //// Setup the decision variables
-            //var xS = CreateSmallVasesVariable(solver, claySupply, glazeSupply);
-            //var xL = CreateLargeVasesVariable(solver, claySupply, glazeSupply);
+            // Setup the decision variables
+            var xS = CreateSmallVasesVariable(solver, claySupply, glazeSupply);
+            var xL = CreateLargeVasesVariable(solver, claySupply, glazeSupply);
 
-            //// Create Constraints
-            //var c0 = CreateClayConstraint(solver, xS, xL, claySupply);
-            //var c1 = CreateGlazeConstraint(solver, xS, xL, glazeSupply);
+            // Create Constraints
+            var c0 = CreateClayConstraint(solver, xS, xL, claySupply);
+            var c1 = CreateGlazeConstraint(solver, xS, xL, glazeSupply);
 
-            //// Find the greatest number of small vases we can make
-            //var maxSmall = System.Math.Min(claySupply, glazeSupply);
+            // Find the greatest number of small vases we can make
+            var maxSmall = System.Math.Min(claySupply, glazeSupply);
 
-            //// Find the greatest number of large vases we can make
-            //var maxLarge = System.Math.Min(claySupply / 4.0, glazeSupply / 2.0);
+            // Find the greatest number of large vases we can make
+            var maxLarge = System.Math.Min(claySupply / 4.0, glazeSupply / 2.0);
 
-            //// Find all feasible combinations of small and large vases
-            //// Note: There are probably several better ways of doing this
-            //// that are more efficient and organic.  For example, we could make
-            //// a tree that represents all of the possible decisions and let the
-            //// optimizer find the solutions from within that tree.
-            //var results = new List<ProductionTarget>();
-            //for (int nSmall = 0; nSmall <= maxSmall; nSmall++)
-            //    for (int nLarge = 0; nLarge <= maxLarge; nLarge++)
-            //    {
-            //        // Force the solution to the target set of values
-            //        var c2 = solver.MakeConstraint(nSmall, nSmall, $"xS_Equals_{nSmall}");
-            //        c2.SetCoefficient(xS, 1);
+            // Add additional constraints to specify potential targets
+            var cS = solver.MakeConstraint(0.0, maxSmall);
+            cS.SetCoefficient(xS, 1);
 
-            //        var c3 = solver.MakeConstraint(nLarge, nLarge, $"xL_Equals_{nLarge}");
-            //        c3.SetCoefficient(xL, 1);
+            var cL = solver.MakeConstraint(0.0, maxLarge);
+            cL.SetCoefficient(xL, 1);
 
-            //        // See if the solution is feasible with those values
-            //        var result = solver.Solve();
-            //        if (result == Solver.OPTIMAL)
-            //            results.Add(new ProductionTarget() { Small = nSmall, Large = nLarge });
 
-            //        solver.Remove(c2);
-            //    }
+            // Find all feasible combinations of small and large vases
+            // Note: There are probably several better ways of doing this
+            // that are more efficient and organic.  For example, we could make
+            // a tree that represents all of the possible decisions and let the
+            // optimizer find the solutions from within that tree.
+            var results = new List<ProductionTarget>();
+            for (int nSmall = 0; nSmall <= maxSmall; nSmall++)
+                for (int nLarge = 0; nLarge <= maxLarge; nLarge++)
+                {
+                    // Force the solution to the target set of values
+                    cS.SetLb(nSmall);
+                    cS.SetUb(nSmall);
+                    cL.SetLb(nLarge);
+                    cL.SetUb(nLarge);
 
-            //return results;
+                    // See if the solution is feasible with those values
+                    var result = solver.Solve();
+                    if (result == Solver.OPTIMAL)
+                        results.Add(new ProductionTarget() { Small = nSmall, Large = nLarge });
+                }
 
-            // TODO: Check out using a DecisionBuilder
-            // https://developers.google.com/optimization/mip/integer_opt_cp
-
-            throw new NotImplementedException();
+            return results;
         }
 
         // This method will work irrespective of the size of the solution space
