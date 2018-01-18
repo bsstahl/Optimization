31dcdfa32e2a62319e2cc326ef3f664b55680130
bsstahl <barry@bsstahl.com>
2018-01-17 21:33:20 -0600

    Add new Microsoft Solver Foundation model to the solution.

---
+++ Bss.Optimization.Sudoku.MSSolverCp/Model.cs
@@ -0,0 +1,18 @@
+﻿using Bss.Optimization.Sudoku.Entities;
+using Bss.Optimization.Sudoku.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bss.Optimization.Sudoku.MSSolverCp
+{
+    public class Model : ISudokuSolver
+    {
+        public IEnumerable<GridCell[]> Solve(IEnumerable<GridCell> hints)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
